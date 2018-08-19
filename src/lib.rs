33bdeeb5937f912f9841197f91477cb97a41ea4c
Yuichi Fujita <cat@y8m.org>
2018-08-20 02:10:13 +0900

    [WIP] Implement many

--- src/lib.rs
+++ src/lib.rs
@@ -13,58 +13,260 @@
 //!     * `vm remove NAME --force` (--forceで確認無しで削除)
 //! * vmの設定をバックアップ。現在日時をファイル名の末尾に付けてバックアップする。バックアップ先は設定ファイルがあるのと同じディレクトリ。
 //!     * `vm backup-config-file`
+//! * 特定のパス以下に存在しているVagrantfileを探してそのパスを表示する
+//!     * `vm find-vagrantfile`
+//! * 任意オプションでvagrantコマンドを実行する。(pluginのコマンド等の非標準コマンドの実行に使用)
+//!     * `vm NAME -c vbguest`
+//!     * `vm NAME -c 'vbguest --do rebuild'`
+//!
+//! ## 将来的に必要性を感じたら作る
 //! * ある名前のvmに対してvagrantコマンドのサブコマンドを実行
 //!     * `vm NAME up`
 //!     * `vm NAME ssh`
 //! * ある名前のvmに対してvagrantコマンドのサブコマンドを引数付きで実行
-//!     * `vm NAME ssh -- -- -L 8080:localhost:8080` (最初の -- 以降のオプションがそのまま渡される)
+//!     * `vm NAME ssh --vagrant-opt '-- -L 8080:localhost:8080'`
+//!     * `vm NAME ssh -o '-- -L 8080:localhost:8080'`
 //! * いくつかのvmコマンドレベルで対応しているvagrantコマンドのサブコマンドの引数を指定して実行
 //!     * `vm NAME ssh -L 8080:localhost:8080`
-//! * 特定のパス以下に存在しているVagrantfileを探してそのパスを表示する
-//!     * `vm find-vagrantfile`
 
 extern crate clap;
-exter crate failure;
+#[macro_use]
+extern crate failure;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate toml;
 
 use failure::Error;
-use std::path::{PathBuf, Path};
-
-/// configファイルの情報
-pub struct Config {
-    vm_list: Vec<Info>,
-}
+use std::collections::BTreeMap;
+use std::ffi::OsStr;
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Read, Write};
+use std::path::{Path, PathBuf};
+use std::process::{Command, ExitStatus};
 
 /// configファイル中のあるVMに関する情報
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Info {
     name: String,
     path: PathBuf,
 }
 
-pub struct Vm {
-    config_file: PathBuf,
-    config: Config,
+impl Info {
+    pub fn name(&self) -> &str {
+        self.name.as_str()
+    }
+
+    pub fn path(&self) -> &Path {
+        self.path.as_path()
+    }
 }
 
-impl Vm {
-    pub fn list(&self) -> Result<Info, Error> {
-        unimplemented!()
+/// configファイルの情報
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Config {
+    vagrant_path: PathBuf,
+    vm_list: BTreeMap<String, Info>,
+}
+
+impl Config {
+    pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Config, Error> {
+        if path.as_ref().exists() {
+            let file = File::open(path)?;
+            let mut buf = Vec::new();
+            let _ = BufReader::new(file).read_to_end(&mut buf)?;
+            Ok(toml::from_slice(&buf)?)
+        } else {
+            let vagrant_path = if cfg!(windows) {
+                "vagrant.exe"
+            } else {
+                "vagrant"
+            };
+            Ok(Config {
+                vagrant_path: PathBuf::from(vagrant_path),
+                vm_list: BTreeMap::new(),
+            })
+        }
     }
 
-    pub fn add<S: AsRef<str>, P: AsRef<Path>>(&self, name: S, path: P) -> Result<(), Error> {
-        unimplemented!()
+    pub fn save_to_file<P: AsRef<Path>>(&self, path: P) -> Result<(), Error> {
+        BufWriter::new(File::create(path)?).write_all(toml::to_string_pretty(self)?.as_bytes())?;
+        Ok(())
+    }
+}
+
+pub trait RunVagrant {
+    fn subcommand<S: AsRef<OsStr>, T: AsRef<OsStr>>(
+        &self,
+        command: S,
+        options: &[T],
+    ) -> Result<ExitStatus, Error>;
+
+    fn raw<S: AsRef<OsStr>>(&self, options: &[S]) -> Result<ExitStatus, Error>;
+}
+
+pub struct Vagrant {
+    path: PathBuf,
+}
+
+impl Vagrant {
+    pub fn new<P: AsRef<Path>>(path: P) -> Vagrant {
+        Vagrant { path: path.as_ref().to_path_buf() }
+    }
+}
+
+impl RunVagrant for Vagrant {
+    fn subcommand<S: AsRef<OsStr>, T: AsRef<OsStr>>(
+        &self,
+        command: S,
+        options: &[T],
+    ) -> Result<ExitStatus, Error> {
+        Ok(Command::new(&self.path)
+            .arg(command.as_ref())
+            .args(options)
+            .status()?)
     }
 
-    pub fn config_file_path(&self) -> Result<PathBuf, Error> {
-        unimplemented!()
+    /// Pass vagrant command options.
+    fn raw<S: AsRef<OsStr>>(&self, options: &[S]) -> Result<ExitStatus, Error> {
+        Ok(Command::new(&self.path).args(options).status()?)
     }
+}
+
+#[derive(Debug, Clone, Fail)]
+#[fail(display = "cannot find {} from vm list in config file", name)]
+pub struct VmInfoFindError {
+    name: String,
+}
+
+pub struct Vm<V: RunVagrant> {
+    config_file_path: PathBuf,
+    config: Config,
+    vagrant: V,
+}
+
+impl<V: RunVagrant> Vm<V> {
+    pub fn new<P: AsRef<Path>>(path: P, vagrant: V) -> Result<Vm<V>, Error> {
+        let config = Config::from_file(path.as_ref())?;
+        Ok(Vm {
+            config_file_path: path.as_ref().to_path_buf(),
+            config,
+            vagrant,
+        })
+    }
+
+    pub fn config_file_path(&self) -> &Path {
+        self.config_file_path.as_path()
+    }
+
+    pub fn config(&self) -> &Config {
+        &self.config
+    }
+
+    pub fn cd<S: AsRef<str>>(&self, name: S) -> Result<(), Error> {
+        let info = self
+            .config
+            .vm_list
+            .get(name.as_ref())
+            .ok_or_else(|| VmInfoFindError {
+                name: name.as_ref().to_string(),
+            })?;
+        std::env::set_current_dir(&info.path)?;
+        Ok(())
+    }
+
+    pub fn list(&self) -> Vec<&Info> {
+        self.config.vm_list.values().collect()
+    }
+
+    /// Add a new entry and return old value that assigned to the `name` if exists.
+    pub fn add<S: AsRef<str>, P: AsRef<Path>>(&mut self, name: S, path: P) -> Option<Info> {
+        let info = Info {
+            name: name.as_ref().to_string(),
+            path: path.as_ref().to_path_buf(),
+        };
+        self.config.vm_list.insert(info.name.clone(), info)
+    }
+
+    pub fn remove<S: AsRef<str>>(&mut self, name: S) -> Option<Info> {
+        self.config.vm_list.remove(name.as_ref())
+    }
+
+    pub fn backup_config_file<P: AsRef<Path>>(&self, path: P) -> Result<(), Error> {
+        let _ = std::fs::copy(self.config_file_path(), path)?;
+        Ok(())
+    }
+
+    fn vagrant<S: AsRef<OsStr>, T: AsRef<OsStr>>(
+        &self,
+        command: S,
+        options: &[T],
+    ) -> Result<ExitStatus, Error> {
+        Ok(self.vagrant.subcommand(command, options)?)
+    }
+
+    /// Pass vagrant command options.
+    fn vagrant_raw<S: AsRef<OsStr>>(&self, options: &[S]) -> Result<ExitStatus, Error> {
+        Ok(self.vagrant.raw(options)?)
+    }
+
+    pub fn get_info<S: AsRef<str>>(&self, name: S) -> Option<&Info> {
+        self.config.vm_list.get(name.as_ref())
+    }
+}
+
+/// Does not traverse symlink.
+pub fn find_vagrantfiles<P: AsRef<Path>>(path: P) -> Result<(), Error> {
+    if !path.as_ref().is_dir() {
+        return Ok(());
+    }
+
+    for result in std::fs::read_dir(path)? {
+        let item = result?;
+        if item.file_name().to_string_lossy() == "Vagrantfile" {
+            println!("{}", item.path().to_string_lossy());
+        } else if item.path().is_dir()
+            && item
+                .path()
+                .metadata()
+                .map(|m| !m.file_type().is_symlink())
+                .unwrap_or(false)
+        {
+            find_vagrantfiles(item.path())?;
+        }
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[cfg(not(windows))]
+    fn vagrant_file_path() -> &'static str {
+        "vagrant"
+    }
+
+    #[cfg(windows)]
+    fn vagrant_file_path() -> &'static str {
+        "vagrant.exe"
+    }
+
+    fn test_config_content_normal() -> String {
+        format!(r#"
+vagrant_file_path = "{}"
+
+[vm_list]
+vm1 = "/home/user/vm/vm1"
+vm2 = "/home/user/vm/vm2"
+        "#, vagrant_file_path())
+    }
+
+    fn create_test_dir() {
+        std::fs::create_dir_all("test").unwrap();
+    }
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
--- src/main/vm.rs
+++ src/main/vm.rs
@@ -1,4 +1,236 @@
+extern crate chrono;
+extern crate clap;
+extern crate directories;
+#[macro_use]
+extern crate failure;
 extern crate vm;
 
-fn main() {
+use directories::ProjectDirs;
+use failure::Error;
+use std::ffi::OsString;
+use std::path::PathBuf;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn main() -> Result<(), Error> {
+    match Args::new(args_parser(), std::env::args()) {
+        Ok(args) => run(&args)?,
+        Err(err) => eprintln!("{}", err),
+    }
+    Ok(())
 }
+
+fn run(args: &Args) -> Result<(), Error> {
+    let config_file_path = config_file_path();
+
+    let vagrant = vm::Vagrant::new(config_file_path.as_path());
+    let mut vm = vm::Vm::new(config_file_path.as_path(), vagrant)?;
+
+    std::fs::create_dir_all(config_file_path.parent().unwrap())?;
+    if !config_file_path.exists() {
+        vm.config().save_to_file(config_file_path.as_path())?;
+    }
+
+    run_vagrant(args, &mut vm)?;
+
+    Ok(())
+}
+
+fn run_vagrant<T: vm::RunVagrant>(args: &Args, vm: &mut vm::Vm<T>) -> Result<(), Error> {
+    match args.subcommand {
+        SubCommand::List => vm
+            .list()
+            .iter()
+            .for_each(|info| println!("{}: {}", info.name(), info.path().to_string_lossy())),
+        SubCommand::Add {
+            name: name,
+            path: path,
+        } => {
+            vm.add(name, path)?;
+            vm.config().save_to_file(vm.config_file_path())?;
+        }
+        SubCommand::Remove {
+            name: name,
+            force: force,
+        } => {
+            if let Some(info) = vm.get_info(name) {
+                print!(
+                    "Delete this entry { name: {}, path: {} } (y/N)",
+                    info.name(),
+                    info.path()
+                );
+                let does_remove = if let Some(result) = std::io::stdin().chars().take(1).next() {
+                    result?.to_lowercase() == 'y'
+                } else {
+                    false
+                };
+                if does_remove {
+                    vm.remove
+                }
+            }
+        }
+        SubCommand::BackupConfigFile => {
+            let t = chrono::Local::now();
+        }
+    }
+
+    unimplemented!()
+}
+
+fn config_file_path() -> PathBuf {
+    let project_dirs =
+        ProjectDirs::from("org", "y8m", "vm").expect("Cannot get config directory path");
+    PathBuf::from(project_dirs.config_dir()).join("config.toml")
+}
+
+struct Args {
+    subcommand: SubCommand,
+}
+
+impl Args {
+    fn new<I, T>(app: clap::App, args: I) -> Result<Args, Error>
+    where
+        I: IntoIterator<Item = T>,
+        T: Into<OsString> + Clone,
+    {
+        let matches = app.get_matches_from_safe(args)?;
+        let subcommand = match matches.subcommand() {
+            ("list", Some(sub_matches)) => SubCommand::List,
+            ("add", Some(sub_matches)) => SubCommand::Add {
+                name: sub_matches
+                    .value_of("add_name")
+                    .ok_or(ArgsError::NoVmName)?
+                    .to_string(),
+                path: PathBuf::from(sub_matches.value_of("add_path").ok_or(ArgsError::NoVmPath)?),
+            },
+            ("remove", Some(sub_matches)) => SubCommand::Remove {
+                name: sub_matches
+                    .value_of("remove_name")
+                    .ok_or(ArgsError::NoVmName)?
+                    .to_string(),
+                force: sub_matches.is_present("remove_force"),
+            },
+            ("backup_config_file", Some(sub_matches)) => SubCommand::BackupConfigFile,
+            ("find_vagrantfiles", Some(sub_matches)) => SubCommand::FindVagrantfiles {
+                base_path: PathBuf::from(PathBuf::from(
+                    sub_matches
+                        .value_of("find_vagrantfiles_base_path")
+                        .ok_or(ArgsError::NoFindVagrantfilesBasePath)?,
+                )),
+            },
+            _ => SubCommand::Raw {
+                vm_name: matches
+                    .value_of("vm_name")
+                    .ok_or(ArgsError::NoVmName)?
+                    .to_string(),
+                options: matches
+                    .value_of("vagrant_options")
+                    .map(|options_str| {
+                        // TODO: 任意パラメータなのでこれだと `'a b'` みたいなのが来るとバグる
+                        options_str
+                            .split_whitespace()
+                            .map(|s| s.to_string())
+                            .collect()
+                    })
+                    .unwrap_or_else(|| Vec::new()),
+            },
+        };
+
+        Ok(Args { subcommand })
+    }
+}
+
+enum SubCommand {
+    List,
+    Add {
+        name: String,
+        path: PathBuf,
+    },
+    Remove {
+        name: String,
+        force: bool,
+    },
+    BackupConfigFile,
+    FindVagrantfiles {
+        base_path: PathBuf,
+    },
+    Raw {
+        vm_name: String,
+        options: Vec<String>,
+    },
+}
+
+#[derive(Debug, Clone, Fail)]
+enum ArgsError {
+    #[fail(display = "Not specified a VM name")]
+    NoVmName,
+    #[fail(display = "Not specified a VM directory path")]
+    NoVmPath,
+    #[fail(display = "Not specified a find path")]
+    NoFindVagrantfilesBasePath,
+}
+
+fn args_parser<'a, 'b>() -> clap::App<'a, 'b> {
+    clap::App::new("vm")
+        .version(VERSION)
+        .author("Yuichi Fujita <cat@y8m.org>")
+        .about("A vagrant wrapper for working directory independent execution.")
+        .after_help("Repository: https://github.com/fujitayy/vm")
+        .arg(
+            clap::Arg::with_name("vm_name")
+                .help("specify a vm name in vm_list of config file")
+                .index(1),
+        )
+        .arg(
+            clap::Arg::with_name("vagrant_options")
+                .short("c")
+                .value_name("VAGRANT_OPTIONS")
+                .help("this value passed to vagrant command")
+                .takes_value(true),
+        )
+        .subcommand(
+            clap::SubCommand::with_name("list").help("Show entries in vm_list of config file"),
+        )
+        .subcommand(
+            clap::SubCommand::with_name("add")
+                .help("Add a entry to vm_list of config file")
+                .arg(
+                    clap::Arg::with_name("add_name")
+                        .help("a name for new vm_list entry")
+                        .value_name("NAME")
+                        .index(1),
+                )
+                .arg(
+                    clap::Arg::with_name("add_path")
+                        .help("a path for new vm_list entry")
+                        .value_name("PATH")
+                        .index(2),
+                ),
+        )
+        .subcommand(
+            clap::SubCommand::with_name("remove")
+                .help("Remove a entry in vm_list of config file specified by this value")
+                .arg(
+                    clap::Arg::with_name("remove_name")
+                        .value_name("NAME")
+                        .index(1),
+                )
+                .arg(
+                    clap::Arg::with_name("remove_force")
+                        .short("f")
+                        .long("force"),
+                ),
+        )
+        .subcommand(clap::SubCommand::with_name("backup_config_file").help("Backup config file"))
+        .subcommand(
+            clap::SubCommand::with_name("find_vagrantfiles")
+                .help("Find Vagrantfiles")
+                .arg(
+                    clap::Arg::with_name("find_vagrantfiles_base_path")
+                        .value_name("PATH")
+                        .index(1),
+                ),
+        )
+}
+
+fn read_config() -> Result<vm::Config, Error> {}
